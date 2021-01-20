b7ec6726e9b54bccd57a30733366a64a9f714319
mohammed salman <mohammeduqu@mohammeds-MacBook-Air.local>
2021-01-20 20:40:27 +0300

    change title
    change title

--- lib/main.dart
+++ lib/main.dart
@@ -73,7 +73,7 @@ class _MyHomePageState extends State<MyHomePage> {
         // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
-      body: Center(child: Text('hello world')),
+      body: Center(child: Text('hello  github')),
     );
   }
 }
